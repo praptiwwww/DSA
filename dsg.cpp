9a48c3210b0c4cf77cd6113065220f9c5e5ccbbe
praptiwwww <149615332+praptiwwww@users.noreply.github.com>
2026-01-10 13:11:37 +0530

    Add files via upload
    printing of array

---
+++ dsg.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+//printing of array
+int main()
+
+{
+    int n;
+    int arr[6] = {2,4,5,9,8,7};
+
+    for(int i = 0; i < 6; i++)
+    {
+        cout<<arr[i]<<"";
+    }
+
+    return 0;
+}
+
+//retruning of the largest element fron the array
